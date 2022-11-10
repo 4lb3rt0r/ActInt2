07cc264ed2125d8f58659b924617daf34f5946f1
Alberto Orozco <80859007+4lb3rt0r@users.noreply.github.com>
2022-11-09 18:58:30 -0600

    Código Inicial

--- ActInt2.cpp
+++ ActInt2.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+struct Nodo {
+    string nombre;
+    int x;
+    int y;
+    bool central;
+};
 
 
 int main () {
 
-    
+    int n, m, k, q;
+    string nombre;
+    Nodo aux;
+    vector<Nodo> colonias;
 
+    cin >> n >> m >> k >> q;
+
+    for (int i = 0; i < n; i++) {
+        cin >> aux.nombre >> aux.x >> aux.y >> aux.central;
+
+        colonias.push_back(aux);
+    }
 
     return 0;
 }
