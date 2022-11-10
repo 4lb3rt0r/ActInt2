dc0dec623a24c1dcbe63ac6d254e8d6e827ae028
Alberto Orozco <80859007+4lb3rt0r@users.noreply.github.com>
2022-11-09 19:22:25 -0600

    Avance Inicial 3

--- ActInt2.cpp
+++ ActInt2.cpp
@@ -24,26 +24,40 @@ struct Edge {
 int main () {
 
     int n, m, k, q, c;
-    string nombre, a, b;
+    string nombre, a, b, c1, c2, name;
     Nodo auxNodo;
+    Nodo colonia;
     vector<Nodo> colonias;
+    vector<Nodo> nuevasColonias;
     vector<Edge> conexiones;
 
     cin >> n >> m >> k >> q;
 
     for (int i = 0; i < n; i++) {
-
         cin >> auxNodo.nombre >> auxNodo.x >> auxNodo.y >> auxNodo.central;
 
         colonias.push_back(auxNodo);
     }
 
     for (int i = 0; i < m; i++) {
-
         cin >> a >> b >> c;
         Edge auxEdge(a, b, c);
 
         conexiones.push_back(auxEdge);
+    }
+
+    for (int i = 0; i < k; i++) {
+        cin >> c1 >> c2;
+
+        // addConnection();
+    }
+
+    for (int i = 0; i < q; i++) {
+        cin >> colonia.nombre >> colonia.x >> colonia.y;
+
+        colonia.central = 0;
+
+        nuevasColonias.push_back(colonia);
     }
 
     return 0;
